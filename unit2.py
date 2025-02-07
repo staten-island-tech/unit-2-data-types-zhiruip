9ab9f1eed05bacaaf65da4661436d7f3ca6d7391
zhiruip <ZHiruip@nycstudents.net>
2025-02-07 10:16:37 -0500

    2/7
    dawd

--- unit2.py
+++ unit2.py
@@ -1,8 +1,23 @@
-x = 3
-y = float(3)
-print(x,y)
+#x = 3
+#y = float(3)
+#print(x,y)
 
-values = [1,2.23,5,7,2,30,15,]
-print(values)
-for i in values:
-    print(values[7])
+#values = [1,2.23,5,7,2,30,15,]
+#print(values)
+#for i in values:
+#    print(values[7])
+
+
+#x = "this is a thing"
+#y= x.split( )
+#z = y[0]
+#print(y)
+#print(z
+
+def ask(x):
+    x = input("enter a sentence: ")
+    y=x.split( )
+    print(f"the total numbers in the input is {len(y)}")
+    
+
+ask(1)
