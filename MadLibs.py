a4391a72fdff7995928d0607683cd2eb7c5fb499
zhiruip <ZHiruip@nycstudents.net>
2025-02-10 10:14:49 -0500

    Madlib 2/10
    a

---
+++ MadLibs.py
@@ -0,0 +1,8 @@
+
+first_verb=input("pick a verb: ")
+second_verb=input("pick a second verb: ")
+first_noun=input("pick a noun: ")
+first_number=input("pick a number: ")
+first_celebrity=input("pick a celebrity: ")
+
+print(f"It was a sunny day, {len(first_celebrity)} was {len(first_verb)} and {len(second_verb)}")
