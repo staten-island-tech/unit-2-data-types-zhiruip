4aa5b701ab078452827380aa08dd12b220b085d5
zhiruip <ZHiruip@nycstudents.net>
2025-02-11 10:16:06 -0500

    tippingaura
    2/11

--- MadLibs.py
+++ MadLibs.py
@@ -4,5 +4,6 @@ second_verb=input("pick a second verb: ")
 first_noun=input("pick a noun: ")
 first_number=input("pick a number: ")
 first_celebrity=input("pick a celebrity: ")
+third_verb = input("pick a third verb: ")
 
-print(f"It was a sunny day, {len(first_celebrity)} was {len(first_verb)} and {len(second_verb)}")
+print(f"It was a sunny day, {first_celebrity} was {first_verb} and {second_verb} on a playground. Suddenly a {first_noun} appeared, it told {first_celebrity} to pick a number. {first_celebrity} was very {third_verb}, {first_celebrity} reached out and picked {first_number}. {first_celebrity} was very lucky, and he gained eternal wealth.")
--- unit2.py
+++ unit2.py
@@ -15,9 +15,30 @@
 #print(z
 
 #def ask(x):
-   # x = input("enter a sentence: ")
-   # y=x.split( )
-   # print(f"the total numbers in the input is {len(y)}")
-    
-
+   #x = input("enter a sentence: ")
+   #y=x.split( )
+   #print(f"the total numbers in the input is {len(y)}")
+   
 #ask(1)
+
+#def oddeven(x):
+    #number = int(input("Input your number: "))  # Make sure to cast input to an integer
+    #if number % 2 == 0:
+        #print(f"{number} is even")
+    #else:  
+        #print(f"{number} is odd")
+#oddeven(1)
+
+def tip_calc(y):
+    tip_percent = input("enter tip :) : ")
+    if tip_percent = "0"
+
+
+def tipping_service(x):
+    bill= input("how much was your bill?:")
+    service = input("was the service, bad, okay, good, or great?: ")
+    if service == "bad":
+        print("sorry! if it was bad, you don't need to tip!")
+        print("would you like to tip, 0%, 15%, 20%, or 25%? ")
+
+      
