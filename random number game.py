c76614728deb163f85e424288bba873e37376d9a
zhiruip <ZHiruip@nycstudents.net>
2025-02-24 10:16:31 -0500

    Bruh code no work 2/24

--- random number game.py
+++ random number game.py
@@ -1,9 +1,17 @@
+import random
+
 
 def gamble():
-    global random_number
-    random_number = range[1,11]
-    x = 
-    print(x)
+    random_number = random.randint(1, 11)
+    guess = int(input("What do you guess: "))
+    while guess == random_number == False:
+        if guess == random_number:
+            print(f"correct! it was {random_number}")
+        else:
+            if guess > random_number:
+                print("wrong! it is less")
+            else:
+                print("wrong! it is more")
 
 
 
