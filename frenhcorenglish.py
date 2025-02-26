f7bb1a013decb7a26f26736bc7f199a48843f9c0
zhiruip <ZHiruip@nycstudents.net>
2025-02-26 10:15:07 -0500

    test 2/26

---
+++ frenhcorenglish.py
@@ -0,0 +1,14 @@
+def englishfrench():
+    sentence = input("what is your text?")
+    for i in sentence == "T" or "t":
+         LettersT + 1
+    for i in sentence == "S" or "s":
+        LettersS + 1
+    if LettersS > LettersT:
+        print("french")
+    else:
+        print("english")
+LettersT = 0
+LettersS = 0
+
+englishfrench()
--- random number game.py
+++ random number game.py
@@ -5,15 +5,15 @@ def gamble():
     global guess
     guess = 0
     random_number = random.randint(1, 11)
-    guess = int(input("What do you guess: "))
-    while guess == random_number is False:
+    guess = int("What do you guess: ")
+    while guess != random_number:
         if guess == random_number:
             print(f"correct! it was {random_number}")
         else:
             if guess > random_number:
-                print("wrong! it is less")
+                input("wrong! it is less, guess again: ")
             else:
-                print("wrong! it is more")
+                input("wrong! it is more, guess again: ")
 
 
 
